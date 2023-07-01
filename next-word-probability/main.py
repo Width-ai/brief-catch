f6d3c68036c0464a65e9b7f2651dc630fedb39d1
pa-t <patrickhennis@gmail.com>
2023-07-01 17:58:36 -0500

    restructure, get all possibilities and return their responses

--- next-word-probability/main.py
+++ next-word-probability/main.py
@@ -4,7 +4,10 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, Form
 from fastapi.responses import JSONResponse
-from src.models.gpt3 import call_gpt3
+from utils.gpt3 import call_gpt3
+from utils.logger import setup_logger
+
+logger = setup_logger()
 
 load_dotenv()
 
@@ -14,18 +17,15 @@ app = FastAPI()
 
 
 @app.post("/generate-next-word")
-def generate_next_word(prompt: str = Form(example="Hello, nice to meet you!"),
+def generate_next_word(input_sentence: str = Form(example="Hello, nice to meet you!"),
              temperature: float = Form(0),
              model: str = Form("text-davinci-003")):
     """
     Generate the next possible words with the log probabilities.
     """
     try:
-        text, words = call_gpt3(prompt, model=model, temperature=temperature)
-        return {
-            "text": text,
-            "words": words
-        }
+        response = call_gpt3(input_sentence, model=model, temperature=temperature)
+        return JSONResponse(content={"likely_words": response})
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
---
+++ next-word-probability/utils/logger.py
@@ -0,0 +1,21 @@
+import logging
+
+def setup_logger():
+    # Create a logger instance
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # Create a formatter for the log messages
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+
+    # Create a console handler for the log messages
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    # logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+    return logger
+
+logger = setup_logger()
