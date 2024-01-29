a52f24aea2a4254cc8de4a663f0863a9ec6db6f5
pa-t <patrickhennis@gmail.com>
2024-01-29 17:02:11 -0500

    fix css tag in ui, add spend file

--- ui-service/pages/Bulk_Rule_Modifying.py
+++ ui-service/pages/Bulk_Rule_Modifying.py
@@ -24,7 +24,7 @@ primaryColor = toml.load(".streamlit/config.toml")['theme']['primaryColor']
 s = f"""
 <style>
 div.stButton > button:first-child {{ background-color: {primaryColor}; border-radius: 5px; color: #fff;}}
-<style>
+</style>
 """
 st.markdown(s, unsafe_allow_html=True)
 
